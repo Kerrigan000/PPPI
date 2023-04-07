788e69ccf0520b3e8cf59237af0accd58e03ff20
Kerrigan000 <dan.vasili4@mail.ru>
2023-04-08 00:28:01 +0300

    Объединил удалённый класс вместе с Классом для генерации цвета блока

--- Команда/GemProg/PPPI/GenBlockColor.cs
+++ Команда/GemProg/PPPI/GenBlockColor.cs
@@ -15,4 +15,19 @@ namespace PPPI
             return block;
         }
     }
+    //Класс генерации формы блока.
+    internal class GenBlockShape
+    {
+        //Генерация формы блока.
+        public Block genShape(Block block)
+        {
+            return block;
+        }
+
+        //Добавление эллемента блока.
+        public Block addPart(Block block)
+        {
+            return block;
+        }
+    }
 }
